f13426a6e0bcfe1e031583d02f496957418226f5
Henry Corse <Kaedraar@gmail.com>
2020-10-30 09:57:19 -0400

    Using the example for g3n, I've managed to make a pair of color shifting cubes. It's a good start

--- main.go
+++ main.go
@@ -1,9 +1,107 @@
 package main
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/g3n/engine/app"
+	"github.com/g3n/engine/camera"
+	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/geometry"
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/graphic"
+	"github.com/g3n/engine/gui"
+	"github.com/g3n/engine/light"
+	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/math32"
+	"github.com/g3n/engine/renderer"
+	"github.com/g3n/engine/util/helper"
+	"github.com/g3n/engine/window"
 )
 
 func main() {
-	fmt.Println("Here goes another signing test")
+
+	// Create application and scene
+	a := app.App()
+	scene := core.NewNode()
+
+	// Set the scene to be managed by the gui manager
+	gui.Manager().Set(scene)
+
+	// Create perspective camera
+	cam := camera.New(1)
+	cam.SetPosition(0, 0, 3)
+	scene.Add(cam)
+
+	// Set up orbit control for the camera
+	camera.NewOrbitControl(cam)
+
+	// Set up callback to update viewport and camera aspect ratio when the window is resized
+	onResize := func(evname string, ev interface{}) {
+		// Get framebuffer size and update viewport accordingly
+		width, height := a.GetSize()
+		a.Gls().Viewport(0, 0, int32(width), int32(height))
+		// Update the camera's aspect ratio
+		cam.SetAspect(float32(width) / float32(height))
+	}
+	a.Subscribe(window.OnWindowSize, onResize)
+	onResize("", nil)
+
+	clrs := make([]*math32.Color, 6)
+	clrs[0] = math32.NewColor("Red")
+	clrs[1] = math32.NewColor("DarkOrange")
+	clrs[2] = math32.NewColor("Yellow")
+	clrs[3] = math32.NewColor("Green")
+	clrs[4] = math32.NewColor("Blue")
+	clrs[5] = math32.NewColor("DarkViolet")
+	mats := make([]*material.Standard, 6)
+
+	// Create a colored cube and add it to the scene
+	geom := geometry.NewCube(1)
+	mesh := graphic.NewMesh(geom, nil)
+	for i := 0; i < 6; i++ {
+		mats[i] = material.NewStandard(clrs[i])
+		mesh.AddGroupMaterial(mats[i], i)
+	}
+	scene.Add(mesh)
+
+	// Create a colored cube and add it to the scene
+	geom2 := geometry.NewCube(1)
+	mesh2 := graphic.NewMesh(geom2, nil)
+	for i := 0; i < 6; i++ {
+		mesh2.AddGroupMaterial(mats[i], i)
+	}
+	scene.Add(mesh2)
+	mesh2.SetPosition(1, 1, 1)
+
+	// keep track of current color offset
+	colOffset := 0
+	// Create and add a button to the scene
+	btn := gui.NewButton("Advance Color")
+	btn.SetPosition(200, 40)
+	btn.SetSize(40, 40)
+	btn.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		colOffset++
+		for i := 0; i < 6; i++ {
+			mats[i].SetColor(clrs[(i+colOffset)%6])
+		}
+	})
+	scene.Add(btn)
+
+	// Create and add lights to the scene
+	scene.Add(light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.8))
+	pointLight := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 5.0)
+	pointLight.SetPosition(1, 0, 2)
+	scene.Add(pointLight)
+
+	// Create and add an axis helper to the scene
+	scene.Add(helper.NewAxes(0.5))
+
+	// Set background color to gray
+	a.Gls().ClearColor(0.5, 0.5, 0.5, 1.0)
+
+	// Run the application
+	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
+		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
+		renderer.Render(scene, cam)
+	})
 }
